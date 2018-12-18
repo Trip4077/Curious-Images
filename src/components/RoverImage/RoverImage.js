6d9357f2248b64ef6370fe654a61d64fc3a0604d
Trip1701 <bj2336@email.vccs.edu>
2018-12-17 23:17:54 -0500

    Image Layout Started

--- src/components/RoverImage/RoverImage.js
+++ src/components/RoverImage/RoverImage.js
@@ -3,9 +3,11 @@ import React from 'react';
 const RoverImage = props => {
   return (
     <div className="image-container">
-      <p>Camera: {props.camera.full_name}</p>
-      <p>Date: {props.date}</p>
-      <p>Photo ID: {props.id}</p>
+      <div className="image-container__text">
+        <p>Camera: {props.camera.full_name}</p>
+        <p>Date: {props.date}</p>
+        <p>Photo ID: {props.id}</p>
+      </div>
       <img src={props.img_src} alt="rover" />
     </div>
   );
